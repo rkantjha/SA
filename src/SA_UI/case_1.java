cfa942a999db9ec4b5606ef00192af23c2a77221
rkantjha <rahul.kant@flipkart.com>
2018-01-15 18:23:17 +0530

    commit with testng

--- src/SA_UI/case_1.java
+++ src/SA_UI/case_1.java
@@ -1,27 +1,34 @@
 package SA_UI;
 
-import javafx.scene.layout.Priority;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pom_elements.login_page;
 
+import static pom_elements.login_page.pass;
+import static pom_elements.login_page.username;
 
 
 public class case_1 {
     WebDriver driver = new FirefoxDriver();
+    login_page lp = new login_page();
 
     @BeforeTest
     public static void start(){
-        System.setProperty("\"webdriver.gecko.driver\", \"/usr/local/bin/geckodriver\"", null);
+        System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
 
     }
 
     @Test(priority = 1,alwaysRun = true)
     public void before_class()
     {
-
+        driver.get(login_page.sa_url);
+        login_page.user_name(driver).sendKeys(username);
+        login_page.verify_me(driver).click();
+        login_page.password(driver).sendKeys(pass);
+        login_page.login_button(driver).click();
+        
     }
 }
--- src/pom_elements/login_page.java
+++ src/pom_elements/login_page.java
@@ -6,26 +6,28 @@ import org.openqa.selenium.WebElement;
 
 public class login_page {
 
-    private String sa_url = "http://10.85.52.152/flipkart/";
+
+    public static String sa_url = "http://10.85.52.152/flipkart/";
     public static String username="rahul.kant";
-    private static WebElement  element =null;
+    public static String pass="Rahul@12345";
+    public static WebElement  element =null;
 
     public static WebElement user_name(WebDriver driver)
     {
         element = driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/div/form/div[1]/input"));
-        return element;
+        return  element;
     }
     public static WebElement verify_me(WebDriver driver)
     {
         element=driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/div/form/div[3]/button"));
         return element;
     }
-    private static WebElement password(WebDriver driver)
+    public static WebElement password(WebDriver driver)
     {
         element=driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/div/form/div[2]/input"));
         return element;
     }
-    private static WebElement login_button(WebDriver driver)
+    public static WebElement login_button(WebDriver driver)
     {
         element=driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/div/form/div[4]/button[1]"));
         return element;
