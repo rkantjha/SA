168ade5cdc470b4e39ad9d6d42c9cb25d8ca79ba
rkantjha <rahul.kant@flipkart.com>
2018-02-13 18:04:05 +0530

    commit with excel file

--- src/pom_elements/order_details.java
+++ src/pom_elements/order_details.java
@@ -1,5 +1,7 @@
 package pom_elements;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,43 +9,50 @@ import org.openqa.selenium.WebElement;
 public class order_details {
 
     public static WebElement element=null;
+    public static XSSFSheet sh;
 
-    public static WebElement ordered_on(WebDriver driver)
+    public static WebElement ordered_on(XSSFWorkbook wb,WebDriver driver)
     {
-        element=driver.findElement(By.xpath(""));
+        sh= wb.getSheetAt(2);
+        element=driver.findElement(By.xpath(sh.getRow(47).getCell(1).getStringCellValue()));
         return element;
     }
-    public static WebElement total_price(WebDriver driver)
+    public static WebElement total_price(XSSFWorkbook wb,WebDriver driver)
     {
-        element=driver.findElement(By.xpath(""));
+        sh= wb.getSheetAt(2);
+        element=driver.findElement(By.xpath(sh.getRow(48).getCell(1).getStringCellValue()));
         return element;
     }
-    public static WebElement channel(WebDriver driver)
+    public static WebElement channel(XSSFWorkbook wb,WebDriver driver)
     {
-        element=driver.findElement(By.xpath(""));
+        sh= wb.getSheetAt(2);
+        element=driver.findElement(By.xpath(sh.getRow(49).getCell(1).getStringCellValue()));
         return element;
     }
-    public static WebElement order_verification(WebDriver driver)
+    public static WebElement order_verification(XSSFWorkbook wb,WebDriver driver)
     {
-        element=driver.findElement(By.xpath(""));
+        sh= wb.getSheetAt(2);
+        element=driver.findElement(By.xpath(sh.getRow(50).getCell(1).getStringCellValue()));
         return element;
     }
-    public static WebElement create_incident(WebDriver driver)
+    public static WebElement create_incident(XSSFWorkbook wb,WebDriver driver)
     {
-        element=driver.findElement(By.xpath(""));
+        sh= wb.getSheetAt(2);
+        element=driver.findElement(By.xpath(sh.getRow(51).getCell(1).getStringCellValue()));
         return element;
     }
-    public static WebElement price_adjustments(WebDriver driver)
+    public static WebElement price_adjustments(XSSFWorkbook wb,WebDriver driver)
     {
-        element=driver.findElement(By.xpath(""));
+        sh= wb.getSheetAt(2);
+        element=driver.findElement(By.xpath(sh.getRow(52).getCell(1).getStringCellValue()));
         return element;
     }
-    public static WebElement cancel_dt(WebDriver driver)
+    public static WebElement cancel_dt(XSSFWorkbook wb,WebDriver driver)
     {
-        element=driver.findElement(By.xpath(""));
+        sh= wb.getSheetAt(2);
+        element=driver.findElement(By.xpath(sh.getRow(53).getCell(1).getStringCellValue()));
         return element;
     }
-
 
 
 }
