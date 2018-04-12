8ccb484756c71c475bcda8699e957e2b448a1f42
rkantjha <rahul.kant@flipkart.com>
2018-04-12 16:00:04 +0530

    commit with excel file

--- src/SA_UI/case_3.java
+++ src/SA_UI/case_3.java
@@ -17,6 +17,10 @@ import pom_elements.discovery_and_authentication;
 import pom_elements.order_wise;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.NoSuchElementException;
 
 
 public class case_3 extends ExcelData {
@@ -25,70 +29,64 @@ public class case_3 extends ExcelData {
   XSSFWorkbook wb = ExcelData.bootstrap();
     public static XSSFSheet sh;
 
-
-
     public case_3() throws IOException {
     }
     @BeforeTest
     public static void BeforeClass() {
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
     }
-    @Test(priority =0,enabled = true)
+
+    @Test(priority =0,enabled = true,groups="three")
     public void login()throws InterruptedException
     {
         one.login();
         synchronized (one.driver) { one.driver.wait(6000); }
     }
+
     @Test(priority=1,enabled=true,groups="three")
-    public void  cashback()  throws InterruptedException
+    public void  cash_back()  throws InterruptedException
     {
-        //click on new session
-        call_to_customer.new_session(one.wb,one.driver).click();
-        synchronized (one.driver) { one.driver.wait(6000); }
+            //click on new session
+            call_to_customer.new_session(one.wb,one.driver).click();
+            synchronized (one.driver) { one.driver.wait(6000); }
 
-        //search for order id
-        WebElement searchBox = discovery_and_authentication.search_box(one.wb, one.driver);
-        synchronized (one.driver) { one.driver.wait(6000); }
+            //search for order id
+            WebElement searchBox = discovery_and_authentication.search_box(one.wb, one.driver);
+            synchronized (one.driver) { one.driver.wait(6000); }
 
-        searchBox.sendKeys(Keys.RETURN);
+            searchBox.sendKeys(Keys.RETURN);
 
-        //name
-        String user_name=discovery_and_authentication.name(one.wb, one.driver,3,1,2).getText();
-        System.out.println(user_name);
-        Assert.assertEquals("Ramki",user_name);
-        //phone
-        String phone_no = discovery_and_authentication.phone(one.wb, one.driver,3,1,3).getText();
-        System.out.println(phone_no +"is the phone number");
-        Assert.assertEquals("+919003276122",phone_no);
-        //email
-        String email_id = discovery_and_authentication.email(one.wb, one.driver,3,1,4).getText();
-        System.out.println(email_id +"is the email id");
-        Assert.assertEquals(email_id,"ramki221994@gmail.com");
-        //account id
-        String account_id = discovery_and_authentication.account_id(one.wb, one.driver,3,1,5).getText();
-        System.out.println(account_id +"account id is verified");
-        Assert.assertEquals(account_id,"ACC14074063501296331");
-        //order details
-        //payment details
-        //address
-        //payment history
-        //dt verification
-        //show selectors(incident,return,replacement,refund,service,callback)
-        //hide selectors
+           //name
+            String user_name = discovery_and_authentication.name(one.wb, one.driver, 3, 1, 2).getText();
+            System.out.println(user_name);
+            Assert.assertEquals("Ramki", user_name);
 
-
-        //close session
-        discovery_and_authentication.close_session(one.wb,one.driver,2,67,1).click();
-
-
-
-
+            //phone
+            String phone_no = discovery_and_authentication.phone(one.wb, one.driver, 3, 1, 3).getText();
+            System.out.println(phone_no + "is the phone number");
+            Assert.assertEquals("+919003276122", phone_no);
+            //email
+            String email_id = discovery_and_authentication.email(one.wb, one.driver, 3, 1, 4).getText();
+            System.out.println(email_id + "is the email id");
+            Assert.assertEquals(email_id, "ramki221994@gmail.com");
+            //account id
+            String account_id = discovery_and_authentication.account_id(one.wb, one.driver, 3, 1, 5).getText();
+            System.out.println(account_id + "account id is verified");
+            Assert.assertEquals(account_id, "ACC14074063501296331");
+            //order details
+            //payment details
+            //address
+            //payment history
+            //dt verification
+            //show selectors(incident,return,replacement,refund,service,callback)
+            //hide selectors
+            //close session
+            discovery_and_authentication.close_session(one.wb, one.driver, 2, 67, 1).click();
     }
     @Test(priority=2,enabled=true,groups="three")
-    public void  ndr()throws InterruptedException
-    {
+    public void  ndr()throws InterruptedException,IOException {
+
         //click on new session
-        call_to_customer.new_session(one.wb,one.driver).click();
         /*
         search
         name,phone,email,account id
@@ -100,13 +98,11 @@ public class case_3 extends ExcelData {
         show selectors(incident,return,replacement,refund,service,callback)
         hide selectors
         */
-
     }
 
-    @Test(priority=4,enabled=true,groups="three")
-    public void  grocerry()throws InterruptedException
+    @Test(priority=3,enabled=true,groups="three")
+    public void  grocery()throws InterruptedException
     {
-        System.out.println("grocerry test passed ");
         //new session
         //search
         //name,phone,email,account id
@@ -119,8 +115,8 @@ public class case_3 extends ExcelData {
         //hide selectors
 
     }
-    @Test(priority=5,enabled=true,groups="three")
-    public void  mps()throws InterruptedException
+    @Test(priority=4,enabled=true,groups="three")
+    public void  Refund()throws InterruptedException
     {
         System.out.println("mps test passed ");
         //new session
@@ -135,8 +131,8 @@ public class case_3 extends ExcelData {
         //hide selectors
 
     }
-    @Test(priority=6,enabled=true,groups="three")
-    public void  doorship()throws InterruptedException
+    @Test(priority=5,enabled=true,groups="three")
+    public void  Blacklisted()throws InterruptedException
     {
         System.out.println("doorship test passed ");
         //new session
@@ -149,10 +145,9 @@ public class case_3 extends ExcelData {
         //dt verification
         //show selectors(incident,return,replacement,refund,service,callback)
         //hide selectors
-
     }
-    @Test(priority=7,enabled=true,groups="three")
-    public void  delivery_installation()throws InterruptedException
+    @Test(priority=6,enabled=true,groups="three")
+    public void  ABB()throws InterruptedException
     {
         System.out.println("delivery_installation test passed ");
         //new session
@@ -166,8 +161,8 @@ public class case_3 extends ExcelData {
         //show selectors(incident,return,replacement,refund,service,callback)
         //hide selectors
     }
-    @Test(priority=8,enabled=true,groups="three")
-    public void  blacklisted()throws InterruptedException
+    @Test(priority=7,enabled=true,groups="three")
+    public void  Ecode()throws InterruptedException
     {
         System.out.println("blacklisted test passed ");
         //new session
@@ -180,10 +175,9 @@ public class case_3 extends ExcelData {
         //dt verification
         //show selectors(incident,return,replacement,refund,service,callback)
         //hide selectors
-
     }
-    @Test(priority=9,enabled=true,groups="three")
-    public void  abb()throws InterruptedException
+    @Test(priority=8,enabled=true,groups="three")
+    public void  ADLD()throws InterruptedException
     {
         System.out.println("abb test passed ");
         //new session
@@ -196,10 +190,9 @@ public class case_3 extends ExcelData {
         //dt verification
         //show selectors(incident,return,replacement,refund,service,callback)
         //hide selectors
-
     }
-    @Test(priority=10,enabled=true,groups="three")
-    public void  ecode()throws InterruptedException
+    @Test(priority=9,enabled=true,groups="three")
+    public void  EGV()throws InterruptedException
     {
         System.out.println("ecode test passed ");
         //new session
@@ -212,10 +205,9 @@ public class case_3 extends ExcelData {
         //dt verification
         //show selectors(incident,return,replacement,refund,service,callback)
         //hide selectors
-
     }
-    @Test(priority=11,enabled=true,groups="three")
-    public void  adld()throws InterruptedException
+    @Test(priority=10,enabled=true,groups="three")
+    public void  BNPL()throws InterruptedException
     {
         System.out.println("adld test passed ");
         //new session
@@ -228,10 +220,9 @@ public class case_3 extends ExcelData {
         //dt verification
         //show selectors(incident,return,replacement,refund,service,callback)
         //hide selectors
-
     }
-    @Test(priority=12,enabled=true,groups="three")
-    public void  egv()throws InterruptedException
+    @Test(priority=11,enabled=true,groups="three")
+    public void  MPS()throws InterruptedException
     {
         System.out.println("egv test passed ");
         //new session
@@ -244,10 +235,9 @@ public class case_3 extends ExcelData {
         //dt verification
         //show selectors(incident,return,replacement,refund,service,callback)
         //hide selectors
-
     }
-    @Test(priority=13,enabled=true,groups="three")
-    public void  non()throws InterruptedException
+    @Test(priority=12,enabled=true,groups="three")
+    public void  Large_Order()throws InterruptedException
     {
         System.out.println("non test passed ");
         //new session
@@ -260,10 +250,9 @@ public class case_3 extends ExcelData {
         //dt verification
         //show selectors(incident,return,replacement,refund,service,callback)
         //hide selectors
-
     }
-    @Test(priority=14,enabled=true,groups="three")
-    public void  associate()throws InterruptedException
+    @Test(priority=13,enabled=true,groups="three")
+    public void  Replacement_back_from_return()throws InterruptedException
     {
         System.out.println("associate test passed ");
         //new session
@@ -276,10 +265,9 @@ public class case_3 extends ExcelData {
         //dt verification
         //show selectors(incident,return,replacement,refund,service,callback)
         //hide selectors
-
     }
-    @Test(priority=15,enabled=true,groups="three")
-    public void  priority()throws InterruptedException
+    @Test(priority=14,enabled=true,groups="three")
+    public void  Dropship()throws InterruptedException
     {
         System.out.println("priority test passed ");
         //new session
@@ -292,10 +280,9 @@ public class case_3 extends ExcelData {
         //dt verification
         //show selectors(incident,return,replacement,refund,service,callback)
         //hide selectors
-
     }
-    @Test(priority=16,enabled=true,groups="three")
-    public void  f_first()throws InterruptedException
+    @Test(priority=15,enabled=true,groups="three")
+    public void  Large()throws InterruptedException
     {
         System.out.println("f_first test passed ");
         //new session
@@ -308,105 +295,9 @@ public class case_3 extends ExcelData {
         //dt verification
         //show selectors(incident,return,replacement,refund,service,callback)
         //hide selectors
-
     }
-    @Test(priority=17,enabled=true,groups="three")
-    public void  ac()throws InterruptedException
-    {
-        System.out.println("ac test passed ");
-        //new session
-        //search
-        //name,phone,email,account id
-        //order details
-        //payment details
-        //address
-        //payment history
-        //dt verification
-        //show selectors(incident,return,replacement,refund,service,callback)
-        //hide selectors
 
-    }
-    @Test(priority=18,enabled=true,groups="three")
-    public void  ndd()throws InterruptedException
-    {
-        System.out.println("ndd test passed ");
-        //new session
-        //search
-        //name,phone,email,account id
-        //order details
-        //payment details
-        //address
-        //payment history
-        //dt verification
-        //show selectors(incident,return,replacement,refund,service,callback)
-        //hide selectors
-
-    }
-    @Test(priority=19,enabled=true,groups="three")
-    public void  bnpl()throws InterruptedException
-    {
-        System.out.println("bnpl test passed ");
-        //new session
-        //search
-        //name,phone,email,account id
-        //order details
-        //payment details
-        //address
-        //payment history
-        //dt verification
-        //show selectors(incident,return,replacement,refund,service,callback)
-        //hide selectors
-
-    }
-    @Test(priority=20,enabled=true,groups="three")
-    public void  multi_ps()throws InterruptedException
-    {
-        System.out.println("multi_ps test passed ");
-        //new session
-        //search
-        //name,phone,email,account id
-        //order details
-        //payment details
-        //address
-        //payment history
-        //dt verification
-        //show selectors(incident,return,replacement,refund,service,callback)
-        //hide selectors
-
-    }
-    @Test(priority=21,enabled=true,groups="three")
-    public void  no_range()throws InterruptedException
-    {
-        System.out.println("no_range test passed ");
-        //new session
-        //search
-        //name,phone,email,account id
-        //order details
-        //payment details
-        //address
-        //payment history
-        //dt verification
-        //show selectors(incident,return,replacement,refund,service,callback)
-        //hide selectors
-
-    }
-    @Test(priority=22,enabled=true,groups="three")
-    public void  service_commands()throws InterruptedException
-    {
-        System.out.println("service_commands test passed ");
-        //new session
-        //search
-        //name,phone,email,account id
-        //order details
-        //payment details
-        //address
-        //payment history
-        //dt verification
-        //show selectors(incident,return,replacement,refund,service,callback)
-        //hide selectors
-
-    }
-    @Test(priority=23,enabled=true,groups="three")
+    @Test(priority=16,enabled=true,groups="three")
     public void  cashback_detail()throws InterruptedException
     {
         System.out.println("cashback_detail test passed ");
@@ -420,10 +311,9 @@ public class case_3 extends ExcelData {
         //dt verification
         //show selectors(incident,return,replacement,refund,service,callback)
         //hide selectors
-
     }
-    @Test(priority=24,enabled=true,groups="three")
-    public void  doorship_and_mps()throws InterruptedException
+    @Test(priority=17,enabled=true,groups="three")
+    public void  door_ship_and_mps()throws InterruptedException
     {
         System.out.println("doorship_and_mps test passed ");
         //new session
@@ -436,7 +326,6 @@ public class case_3 extends ExcelData {
         //dt verification
         //show selectors(incident,return,replacement,refund,service,callback)
         //hide selectors
-
     }
     @AfterTest(enabled = true,groups="three")
     public void close_and_quit() {
